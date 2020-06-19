d45145b21e21fa62bf61f42681f26501af919005
DavidPena <dpena@kirvit.com>
2020-06-19 11:55:28 -0500

    Elimine dos proyectos que causaban conflicto

---
+++ Login/Logica/LogUsuario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.Models;
+
+namespace Login.Logica
+{
+    public class LogUsuario
+    {
+        KIRVIT_AppContext db;
+        public LogUsuario()
+        {
+            db = new KIRVIT_AppContext();
+        }
+
+        public Persona LVerificarUsuario(string txtUser, string txtPassword)
+        {
+
+            PermissionPolicyUser Usuario = db.PermissionPolicyUser.First();
+
+            if (Usuario != null)
+            {
+                Persona UsuarioRegistrado = db.Persona.FirstOrDefault(x => x.Oid.Equals(Usuario.Oid));
+                string password = Usuario.StoredPassword;
+                if (password == txtPassword)
+                {
+                    return UsuarioRegistrado;
+                }
+                else
+                {
+                    Console.WriteLine("Contraseña incorrecta " + DateTime.Now.ToString());
+                    return null;
+                  
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usuario incorrecto " + DateTime.Now.ToString());
+                return null;
+            }
+        }
+
+    }
+}
