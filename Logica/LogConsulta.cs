732eb6d19481a11dfae0b50a1f55279e85108641
DavidPena <dpena@kirvit.com>
2020-06-19 15:27:25 -0500

    Inicio consultas

---
+++ Logica/LogConsulta.cs
@@ -0,0 +1,74 @@
+﻿using Datos.Models;
+using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class LogConsulta
+    {
+        KIRVIT_AppContext db;
+        
+
+        public LogConsulta()
+        {
+            db = new KIRVIT_AppContext();
+        }
+
+        public List<Curso> GetListaCursos(Persona PersonaLogeada)
+        {
+            
+            var consulta = from p in db.PersonaEstudiantesGrupoGruposEstudiantes
+                           where p.Estudiantes.Equals(PersonaLogeada.Oid)
+                           join g in db.Grupo on p.GruposEstudiantes equals g.Oid
+                           join c in db.Curso on g.CursoAsociado equals c.Oid
+                           select c;
+
+            return consulta.ToList();          
+            
+          }
+
+       
+        public List<Grupo> GetListaGrupos(Persona PersonaLogeada) {
+
+
+            var consulta = from p in db.PersonaEstudiantesGrupoGruposEstudiantes
+                           where p.Estudiantes.Equals(PersonaLogeada.Oid)
+                           join g in db.Grupo on p.GruposEstudiantes equals g.Oid
+                           join c in db.Curso on g.CursoAsociado equals c.Oid
+                           select g;
+
+            return consulta.ToList();
+        }
+
+        public List<MateriasTeóricas> GetListaMateriasTeoricas(Persona PersonaLogeada) {
+            var consulta = from p in db.PersonaEstudiantesGrupoGruposEstudiantes
+                           where p.Estudiantes.Equals(PersonaLogeada.Oid)
+                           join g in db.Grupo on p.GruposEstudiantes equals g.Oid
+                           join c in db.Curso on g.CursoAsociado equals c.Oid
+                           join m in db.MateriasTeóricasMateriasTeóricasCursoCursos on c.Oid equals m.Cursos
+                           join t in db.MateriasTeóricas on m.MateriasTeóricas equals t.Oid
+                           select t;
+
+            return consulta.ToList();
+        }
+        public List<MateriasPrácticas> GetListaMateriasPracticas(Persona PersonaLogeada) {
+            var consulta = from p in db.PersonaEstudiantesGrupoGruposEstudiantes
+                           where p.Estudiantes.Equals(PersonaLogeada.Oid)
+                           join g in db.Grupo on p.GruposEstudiantes equals g.Oid
+                           join c in db.Curso on g.CursoAsociado equals c.Oid
+                           join m in db.MateriasPrácticasMateriasPrácticasCursoCursos on c.Oid equals m.Cursos
+                           join t in db.MateriasPrácticas on m.MateriasPrácticas equals t.Oid
+                           select t;
+            return consulta.ToList();
+        }
+
+
+
+    }
+}
---
+++ Logica/LogIngreso.cs
@@ -0,0 +1,57 @@
+﻿using Datos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class LogIngreso
+    {
+        private KIRVIT_AppContext db;
+        
+        public LogIngreso() 
+        {
+            db = new KIRVIT_AppContext();        
+        }
+
+
+        public Persona ValidarUsuario(string txt_user, string txt_password) {
+            PermissionPolicyUser UsuarioDatos = db.PermissionPolicyUser.FirstOrDefault(x => x.UserName.Equals(txt_user));
+
+            if (UsuarioDatos != null)
+            {
+
+                if (UsuarioDatos.StoredPassword == null) {
+                    if (txt_password == "")
+                    {
+                        return db.Persona.FirstOrDefault(x => x.Oid.Equals(UsuarioDatos.Oid));
+
+                    }
+                    else 
+                    {
+                        return null;
+                    }
+                
+                }
+                else if (UsuarioDatos.StoredPassword.Equals(txt_password))
+                {
+                    return db.Persona.FirstOrDefault(x => x.Oid.Equals(UsuarioDatos.Oid));
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            else {
+
+                return null;
+            }
+                
+        }
+
+
+    }
+}
